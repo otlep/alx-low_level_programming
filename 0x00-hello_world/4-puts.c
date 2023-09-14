bd13693aab843b44bef95f901722bac2300ad77e
otlep <oaudi.lephunya@gmail.com>
2023-09-14 10:38:45 -0700

    C program that prints a new line followed by a line

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Program that prints a line with puts function
+ * Return: 0 (succesful)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
